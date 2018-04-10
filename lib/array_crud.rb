96dad8aeb294109ad0a80e25acbc1a8d31438e1d
Eric Fiorani <efiorani305@gmail.com>
2018-04-10 19:03:58 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,7 +23,7 @@ end
 
 def remove_element_from_start_of_array(array)
   array = ["wow", "a", "b", "c", "d", "arrays!"]
-  array.pop(0)
+  array.delete_at(0)
 end
 
 def retrieve_element_from_index(array, index_number)
